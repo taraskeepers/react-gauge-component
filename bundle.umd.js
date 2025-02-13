aa4d8bebb5b508c46f4ea0bac934dd93defd2666
ppcexperts <ppc.experts.group@gmail.com>
2025-02-13 12:07:49 -0700

    Add files via upload

--- bundle.umd.js
+++ bundle.umd.js
@@ -1,20 +1,19 @@
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('react'), require('react-dom'), require('react-gauge-component')) :
   typeof define === 'function' && define.amd ? define(['react', 'react-dom', 'react-gauge-component'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.React, global.ReactDOM, global.ReactGaugeComponent));
-})(this, (function (React, ReactDOM, GaugeComponent) { 'use strict';
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.React, global.ReactDOM, global.reactGaugeComponent));
+})(this, (function (React, ReactDOM, reactGaugeComponent) { 'use strict';
 
   function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
 
   var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
   var ReactDOM__default = /*#__PURE__*/_interopDefaultLegacy(ReactDOM);
-  var GaugeComponent__default = /*#__PURE__*/_interopDefaultLegacy(GaugeComponent);
 
   // Generate a random test value between 0 and 100
   const randomValue = Math.floor(Math.random() * 101);
 
   // Render the gauge into an element with id "myGauge"
-  ReactDOM__default["default"].render( /*#__PURE__*/React__default["default"].createElement(GaugeComponent__default["default"], {
+  ReactDOM__default["default"].render( /*#__PURE__*/React__default["default"].createElement(reactGaugeComponent.GaugeComponent, {
     value: randomValue,
     type: "radial",
     arc: {
