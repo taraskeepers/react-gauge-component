fe8fc288e8cd5760e6543721ab9710c369ec6196
ppcexperts <ppc.experts.group@gmail.com>
2025-02-13 11:33:11 -0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,24 +2,27 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 
+const extensions = ['.js', '.jsx'];
+
 export default {
-  input: 'src/index-wix.js', // our custom entry file
+  input: 'src/index-wix.js',
   output: {
-    file: 'dist/bundle.umd.js', // bundled output file
+    file: 'dist/bundle.umd.js',
     format: 'umd',
-    name: 'MyGaugeApp', // the global variable name for your bundle
+    name: 'MyGaugeApp',
     globals: {
       react: 'React',
       'react-dom': 'ReactDOM',
       'react-gauge-component': 'ReactGaugeComponent'
     }
   },
-  external: ['react', 'react-dom', 'react-gauge-component'], // these are provided externally
+  external: ['react', 'react-dom', 'react-gauge-component'],
   plugins: [
-    resolve(),
+    resolve({ extensions }),
     commonjs(),
     babel({
       babelHelpers: 'bundled',
+      extensions, // This tells Babel to process .js and .jsx files
       exclude: 'node_modules/**'
     })
   ]
